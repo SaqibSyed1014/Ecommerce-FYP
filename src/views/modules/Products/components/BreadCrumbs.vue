8f5ec7054921475ee18d1f533e557358a4842b31
Saqib Syed <sa@techscale.io>
2023-03-06 01:07:54 +0500

    resolved: breadcrumb fix

--- src/views/modules/Products/components/BreadCrumbs.vue
+++ src/views/modules/Products/components/BreadCrumbs.vue
@@ -20,3 +20,21 @@ export default {
   name: 'BreadCrumbs',
 }
 </script>
+
+<style scoped lang="scss">
+.breadcrumb-wrapper{
+  padding: .5rem 0;
+  border-bottom: 0.1rem solid hsla(0,0%,92.2%,.55);
+  .breadcrumb{
+    padding: .3rem 0;
+    .breadcrumb-item{
+      font-weight: 500;
+      & a{
+        color: #777;
+        &:hover{ color: var(--primary); }
+      }
+      &.active{ color: var(--primary); }
+    }
+  }
+}
+</style>
